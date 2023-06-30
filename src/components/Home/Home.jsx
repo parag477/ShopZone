c5d08c957f9aff9206a6eaa4a9564dc928e4305e
Parag <paragagrawal477@gmail.com>
2023-06-30 21:04:56 +0530

    Completed Product Section 🍕

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,14 @@
 import "./Home.scss";
 import Banner from "./Banner/Banner";
 import Category from "./Category/Category";
-
+import Products from "../Products/Products";
 const Home = () => {
     return <div>
         <Banner />
         <div className="main-content">
             <div className="layout">
                 <Category />
+                <Products />
             </div>
         </div>
     </div>;
--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -1,6 +1,18 @@
 import "./Product.scss";
+import Prod from "../../../assets/products/earbuds-prod-1.webp"
+
 const Product = () => {
-    return <div>Product</div>;
+    return (
+        <div className="product-card">
+            <div className="thumbnail">
+                <img src={Prod} alt="" />
+            </div>
+            <div className="prod-details">
+                <div className="name">Product Name</div>
+                <div className="price">&#8377;499</div>
+            </div>
+        </div>
+    );
 };
 
 export default Product;
