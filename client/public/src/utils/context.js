11070d45104486fafdf9c9d823cbab6d7e84ccd0
Parag <paragagrawal477@gmail.com>
2023-07-06 13:37:31 +0530

    Functionalities Completed 💥

--- client/public/src/utils/context.js
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-
-export const Context = createContext();
-
-const AppContext = ({ children }) => {
-    return <Context.Provider>{children}</Context.Provider>;
-};
-
-export default AppContext;
---
+++ client/src/components/Products/Product/Product.jsx
@@ -0,0 +1,20 @@
+import "./Product.scss";
+import { useNavigate } from "react-router-dom";
+
+const Product = ({ id, data }) => {
+    const navigate = useNavigate();
+
+    return (
+        <div className="product-card" onClick={()=> navigate("/product/" + id)}>
+            <div className="thumbnail">
+                <img src={process.env.REACT_APP_DEV_URL + data.img.data[0].attributes.url} alt="" />
+            </div>
+            <div className="prod-details">
+                <div className="name">{data.title}</div>
+                <div className="price">&#8377;{data.price}</div>
+            </div>
+        </div>
+    );
+};
+
+export default Product;
