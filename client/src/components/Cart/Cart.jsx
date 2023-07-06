72fcb671ba94f7d62147505fa220120b4d94198e
Parag <paragagrawal477@gmail.com>
2023-07-06 14:15:15 +0530

    Stripe Integration 🎨

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -5,9 +5,28 @@ import CardItem from "./CartItem/CartItem";
 import "./Cart.scss";
 import { useContext } from "react";
 import { Context } from "../../utils/context";
+import { loadStripe } from "@stripe/stripe-js";
+import { makePaymentRequest } from "../../utils/api";
 
 const Cart = ({ setShowCart }) => {
     const { cartItems, cartSubTotal } = useContext(Context)
+
+    const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY)
+
+    const handlePayment = async () => {
+        try {
+            const stripe = await stripePromise;
+            const res = await makePaymentRequest.post("/api/orders", {
+                products: cartItems,
+            });
+    
+            await stripe.redirectToCheckout({
+                sessionId: res.data.stripeSession.id
+            })
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
     return (
     <div className="cart-panel">
@@ -35,7 +54,7 @@ const Cart = ({ setShowCart }) => {
                         <span className="text total">&#8377;{cartSubTotal}</span>
                     </div>
                     <div className="button">
-                        <button className="checkout-cta">Checkout</button>
+                        <button className="checkout-cta" onClick={handlePayment} >Checkout</button>
                     </div>
                 </div>
             </>}
--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -15,3 +15,10 @@ export const fetchDataFromApi = async (url) => {
         return error;
     }
 };
+
+export const makePaymentRequest = axios.create({
+    baseURL: process.env.REACT_APP_DEV_URL, 
+    headers: {
+        Authorization: "bearer" + process.env.REACT_APP_STRAPI_APP_KEY,
+    },
+})
